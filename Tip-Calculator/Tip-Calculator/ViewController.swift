ef7b09aa7f2fe9893615e15bc08dd156091bcf0e
Erik Batista <erikbatista42@gmail.com>
2017-02-22 00:32:44 -0500

    added files and objects

---
+++ Tip-Calculator/Tip-Calculator/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Tip-Calculator
+//
+//  Created by Erik Batista on 2/21/17.
+//  Copyright © 2017 com.hackclub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billAmountField: UITextField!
+    
+    @IBOutlet weak var tipSelector: UISegmentedControl!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
