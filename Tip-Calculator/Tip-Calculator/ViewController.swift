006e1cfcc6fba9480ca80b77f19fa817ab69c17c
Erik Batista <erikbatista42@gmail.com>
2017-02-23 02:15:09 -0500

    working tip calculator

--- Tip-Calculator/Tip-Calculator/ViewController.swift
+++ Tip-Calculator/Tip-Calculator/ViewController.swift
@@ -14,8 +14,39 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tipSelector: UISegmentedControl!
     
+    @IBOutlet weak var tipAmountField: UITextField!
     
+    @IBOutlet weak var totalAnountField: UITextField!
     
+    @IBAction func calculateTip(_ sender: UIButton) {
+        
+        if let billAmount  = Double(billAmountField.text!) {
+            var tipPercentage = 0.0
+            
+            switch tipSelector.selectedSegmentIndex {
+            case 0: tipPercentage = 0.15
+            case 1: tipPercentage = 0.18
+            case 2: tipPercentage = 0.20
+            default: break
+            }
+            let roundedBillAmount = round(100 * billAmount) / 100
+            let tipAmount = roundedBillAmount * tipPercentage
+            let roundedTipAmount = round(100*tipAmount) / 100
+            let totalAmount = roundedBillAmount + roundedTipAmount
+            
+            if (!billAmountField.isEditing) {
+                billAmountField.text = String(format: "%.2f", roundedBillAmount)
+            }
+            tipAmountField.text = String(format: "%.2f", roundedTipAmount)
+            totalAnountField.text = String(format: "%.2f", totalAmount)
+        } else {
+            //show error
+            billAmountField.text = ""
+            tipAmountField.text = ""
+            totalAnountField.text = ""
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
